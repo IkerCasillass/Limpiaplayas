a66dfb43d720c60f9ab28d499107109cdacfbb62
Liz204 <100331027+Liz204@users.noreply.github.com>
2023-02-16 23:39:04 -0600

    Add files via upload

---
+++ serialconnect.py
@@ -0,0 +1,12 @@
+import serial
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
+    ser.flush()
+    
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+            if float(line) < 23:
+                ser.write(b"on\n")
