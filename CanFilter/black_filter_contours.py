954c22f2f1c74b64465e322019274b328d0b2755
GinfinitFC <giank.francocarrillo@gmail.com>
2023-03-22 15:55:45 -0600

    newTests

--- CanFilter/black_filter_contours.py
+++ CanFilter/black_filter_contours.py
@@ -33,8 +33,42 @@ def kmeans(image):
 def nothing(val):
     pass
 
-vc = cv.VideoCapture(0)
+def video_analisis(frame):
+    frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV) # Transformar BGR a HSV
+        
+    blackMask = cv.inRange(frameHSV, Black, Black1)
 
+    nothing(0) # Iniciar trackbar
+
+    #B = cv.getTrackbarPos('Black', 'Frame')
+    #maxBlackValue = B
+
+    maxBlackValue = int(cv.getTrackbarPos('Black', 'frame')) #Utilizar el valor decicido en la trackbar
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    ret, blackDetection = cv.threshold(gray, maxBlackValue, 255, cv.THRESH_BINARY_INV) #Detectar valores menores a 20
+    #print(maxBlackValue)
+    visualizeBlues = cv.bitwise_and(frame, frame, mask=blackMask)
+
+    kernel = np.ones((10, 10), np.uint8)
+    opening = cv.morphologyEx(blackDetection, cv.MORPH_OPEN, kernel, iterations=1)
+    closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+
+    contours, hierarchy = cv.findContours(blackDetection, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    frame = cv.drawContours(frame, contours, -1,(0,200,0),3) #Agregando contornos del color negro al frame original    
+
+    #K = kmeans(opening)
+    #cv.imshow("Kmeans", K)
+    
+    cv.imshow('frame', frame)
+    cv.imshow("Negro", blackDetection)
+    #cv.imshow("open", closing)
+
+#MAIN
+vc = cv.VideoCapture(0)
+#vc = cv.imread("./lata_playa.jpg")
+#resized = cv.resize(vc,(720,500))
+#blur = cv.GaussianBlur(resized,(7,7),cv.BORDER_DEFAULT)
 Black = np.array([0, 0, 0], np.uint8)
 Black1 = np.array([0, 0, 30], np.uint8)
 
@@ -48,43 +82,16 @@ cv.createTrackbar(trackbar_name, "frame" , 0, slider_Max, nothing)
 while True:
     ret, frame = vc.read()
     if ret == True:
-        frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV) # Transformar BGR a HSV
-        
-        blackMask = cv.inRange(frameHSV, Black, Black1)
-
-        nothing(0) # Iniciar trackbar
-
-        #B = cv.getTrackbarPos('Black', 'Frame')
-        #maxBlackValue = B
-
-        maxBlackValue = int(cv.getTrackbarPos('Black', 'frame')) #Utilizar el valor decicido en la trackbar
-
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        ret, blackDetection = cv.threshold(gray, maxBlackValue, 255, cv.THRESH_BINARY_INV) #Detectar valores menores a 20
-        print(maxBlackValue)
-        visualizeBlues = cv.bitwise_and(frame, frame, mask=blackMask)
-
-        kernel = np.ones((3, 3), np.uint8)
-        opening = cv.morphologyEx(blackDetection, cv.MORPH_OPEN, kernel, iterations=1)
-        closing = cv.morphologyEx(visualizeBlues, cv.MORPH_CLOSE, kernel)
-
-        contours, hierarchy = cv.findContours(blackDetection, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        frame = cv.drawContours(frame, contours, -1,(0,200,0),3) #Agregando contornos del color negro al frame original
-
-        
-        
-
-        
-
-        #K = kmeans(opening)
-        #cv.imshow("Kmeans", K)
-
-        cv.imshow('frame', frame)
-        #cv.imshow("Negro", blackDetection)
-        cv.imshow("open", opening)
+        video_analisis(frame)
+        #get_blob_centroid(frame)
 
         if cv.waitKey(50) == 27:
             break
 
 vc.release()
+'''while True:
+    #video_analisis(blur)
+    get_blob_centroid(blur)
+    if cv.waitKey(50) == 27:
+        break'''
 cv.destroyAllWindows()
