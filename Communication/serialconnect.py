ac8bf2a1026cec6b190577ed92e4a0ebe193d01f
GinfinitFC <giank.francocarrillo@gmail.com>
2023-04-24 22:04:30 -0600

    communicationTest

--- Communication/serialconnect.py
+++ Communication/serialconnect.py
@@ -6,19 +6,24 @@
 import serial,time
 
 # Serial communication
-def arduinoMessage(cmd, arduino):
+def arduinoMessage(cmd):
      
-     #print("{} connected!".format(arduino.port))
-     try:
-          arduino.write(cmd.encode())
-          time.sleep(0.1) #wait for arduino to answer
-          while arduino.inWaiting()==0: pass
-          if  arduino.inWaiting()>0: 
-               answer=arduino.readline()
-               print(answer)
-               arduino.flushInput() #remove data after reading
-     except KeyboardInterrupt:
-          print("KeyboardInterrupt has been caught.")
+    with serial.Serial("/dev/ttyAMA0", 9600, timeout=1) as arduino:
+        time.sleep(0.1) #wait for serial to open
+        if arduino.isOpen():
+            print("{} connected!".format(arduino.port))
+            try:
+                while True:
+                    arduino.write(cmd.encode())
+                    #time.sleep(0.1) #wait for arduino to answer
+                    while arduino.inWaiting()==0: pass
+                    if  arduino.inWaiting()>0: 
+                        answer=arduino.readline()
+                        print(answer)
+                        arduino.flushInput() #remove data after reading
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt has been caught.")
+
 
 def main():
 
@@ -26,26 +31,18 @@ def main():
     msg = "led0"
     # Arduino port
     # with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-    arduino = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
     time.sleep(0.1) #wait for serial to open
 
-    #check for conection
-    if arduino.isOpen():
-        print("{} connected!".format(arduino.port))
-
     #Main loop
-    while arduino.isOpen():
-        try:
-            if msg == "led0":
-                msg = "led1"
-                arduinoMessage(msg)
-                time.sleep(2.0)
-            else:
-                msg == "led0"
-                arduinoMessage(msg)
-                time.sleep(1.0)
-        except KeyboardInterrupt:
-          print("KeyboardInterrupt has been caught.")
+    while True:
+        if msg == "led0":
+            msg = "led1"
+            arduinoMessage(msg)
+            time.sleep(2.0)
+        else:
+            msg == "led0"
+            arduinoMessage(msg)
+            time.sleep(1.0)
 
 if __name__ == '__main__':
     main()
