d656884c4434946c37b357c792c825d0be65fc69
GinfinitFC <giank.francocarrillo@gmail.com>
2023-04-25 09:59:48 -0600

    messages

--- Tests/functions2.py
+++ Tests/functions2.py
@@ -272,9 +272,9 @@ def depositHoop(x,y,x_inflim, x_suplim):
                
      return instruction,visionCan
 
-def arduinoMessage(cmd, arduino):
+def arduinoMessage(cmd):
 
-     # with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+      with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
           time.sleep(0.1) #wait for serial to open
           if arduino.isOpen():
                print("{} connected!".format(arduino.port))
